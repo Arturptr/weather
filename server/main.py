078be99712febd501c2fc0b9f190c9791d4369c8
Arturptr <archifortik@gmail.com>
2025-10-02 18:11:46 +0500

    Sayat server

---
+++ server/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()   # ⚡ Обязательно переменная с именем "app"
+
+@app.get("/")
+def root():
+    return {"message": "Привет! Сервер работает 🚀"}
+@app.get("/forecast")
+def get_forecast(city: str, date: str):
+    return {
+        "city": city,
+        "date": date,
+        "forecast": {
+            "rain_probability": 0.7,
+            "temperature": {"min": 20, "max": 28},
+            "wind_speed": 10
+        }
+    }
